e365052b7ac18a44be55118db8fe4ab7c575a95d
Wolf Developer <lupo.andrea.93@gmail.com>
2022-12-03 18:59:56 +0100

    LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 import { CharactersCollections } from "./CharactersCollections";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 30, -5, 11]);
 
@@ -13,3 +14,14 @@ const charactersCollection = new CharactersCollections('Xassdvvxcv');
 const charSorter = new Sorter(charactersCollection);
 charSorter.sort();
 console.log(charactersCollection.data);
+
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(3);
+linkedList.add(14);
+
+const listSorter = new Sorter(linkedList);
+listSorter.sort();
+linkedList.print();
